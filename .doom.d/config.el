2fba3b1ed7de6603ed3b80a52aed947551ca68c2
Daniel Liao <liao.daniel89@gmail.com>
2019-11-11 19:43:54 +1100

    [master] Add gotham theme

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -12,6 +12,10 @@
 ;; Reload existing files changed outside of emacs
 (global-auto-revert-mode t)
 
+;; Set default theme
+(setq doom-theme 'gotham)
+
+;; Cats
 (use-package nyan-mode
    :custom
    (nyan-animate-nyancat t)
@@ -21,5 +25,5 @@
 (map!
   :leader
   "j" nil
-  (:desc "GoToChar" "j" #'evil-avy-goto-char-timer))
+  (:desc "Go To Char All Windows" "j" #'evil-avy-goto-char-timer))
 
